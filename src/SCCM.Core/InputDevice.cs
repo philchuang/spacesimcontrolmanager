ddbf83ebe3fd6f52054d6acedf8964d766d3efc8
Phil Chuang <chublogga@gmail.com>
2022-12-21 10:52:01 -0500

    got tests working

--- src/SCCM.Core/InputDevice.cs
+++ src/SCCM.Core/InputDevice.cs
@@ -5,4 +5,5 @@ public class InputDevice
     public string? Type { get; set; }
     public int Instance { get; set; }
     public string? Product { get; set; }
+    public IList<InputDeviceSetting> Settings { get; set; } = new List<InputDeviceSetting>();
 }
