96560b4f52b03d15594fe549aea30c47cd64dbd4
Phil Chuang <chublogga@gmail.com>
2022-12-25 20:33:43 -0500

    implementing merger tests

---
+++ src/SCCM.Tests/Extensions.cs
@@ -0,0 +1,14 @@
+using Newtonsoft.Json;
+
+namespace SCCM.Tests;
+
+public static class Extensions
+{
+    public static T JsonCopy<T>(this T self)
+    {
+        if (self == null) throw new ArgumentNullException(nameof(self));
+        var json = JsonConvert.SerializeObject(self);
+        var clone = JsonConvert.DeserializeObject<T>(json);
+        return clone;
+    }
+}
