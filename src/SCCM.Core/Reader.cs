70d4c7e5fdb2c9129c47db2e3f2f4651bb435068
Phil Chuang <dev@philchuang.com>
2023-01-19 02:54:14 -0500

    initial checkin

---
+++ src/SCCM.Core/Reader.cs
@@ -0,0 +1,6 @@
+﻿namespace SCCM.Core;
+
+public class Reader
+{
+
+}
---
+++ src/SCCM.cli/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.CommandLine;
+using SCCM.Core;
+
+namespace SCCM.cli;
+
+class Program
+{
+    private static Command BuildRootCommand(SCCM.Core.Mapper mapper)
+    {
+        var root = new RootCommand("Star Citizen Control Mapper Tool");
+        root.AddCommand(BuildReadCommand(mapper));
+        root.AddCommand(BuildWriteCommand(mapper));
+        return root;
+    }
+
+    private static Command BuildReadCommand(SCCM.Core.Mapper mapper)
+    {
+        var cmd = new Command("copy", "Reads in the current Star Citizen control mappings and saves it locally.");
+        cmd.SetHandler(async () => {
+            await mapper.Read();
+        });
+        return cmd;
+    }
+
+    private static Command BuildWriteCommand(SCCM.Core.Mapper mapper)
+    {
+        var cmd = new Command("paste", "Updates the Star Citizen control mappings from the local copy.");
+        cmd.SetHandler(async () => {
+            await mapper.Write();
+        });
+        return cmd;
+    }
+
+    static async Task<int> Main(string[] args)
+    {
+        var mapper = new Mapper();
+        var root = BuildRootCommand(mapper);
+        return await root.InvokeAsync(args);
+    }
+}
